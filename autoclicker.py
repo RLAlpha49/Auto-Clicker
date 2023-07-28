9c641f5aef899dc96271323116409cce14f28e33
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2023-07-28 11:41:02 -0500

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -4,17 +4,17 @@ import mouse
 
 ###############################################################
 '''
-DISCLAIMER: If you choose to set the CPS to a higher value than 
-my 1000 limit then you need to get rid of the "cps = cps_speed()" 
-function in the main program. As well as take out the 'cps' 
-parameter from the 'custom_event_loop' function. Lastly you would
+DISCLAIMER: If you choose to set the CPS to a higher value than
+my 1000 limit, then you need to get rid of the "cps = cps_speed()"
+function in the main program. As well as take out the "cps"
+parameter from the 'custom_event_loop' function. Lastly, you would 
 need to switch the 'cps' variable in "time.sleep(cps)" to your
-desired cps. Note that it needs to be in seconds, for example 
+desired cps. Note that it needs to be in seconds. For example,
 "0.001" for 1000 CPS and "0.0001" for 10000 CPS.
 
-I Chose the limit of 1000 because at some point programs just 
-dont accept more clicks. Websites especially lag/freeze alot 
-when going to fast.
+I Chose the limit of 1000 because, at some point, programs just
+don't accept more clicks. Websites especially, lag or freeze a lot.
+when going fast.
 '''
 ###############################################################
 
