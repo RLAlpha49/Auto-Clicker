c63ada6473faf48b15c69c8c1ab2ac6dfa66f069
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2023-07-30 03:39:36 -0500

    Update autoclicker.py
    Added 5 preset CPS options

--- autoclicker.py
+++ autoclicker.py
@@ -24,15 +24,37 @@ listener = True
 
 # Function to set the desired clicks per second (CPS) speed
 def cps_speed():
+    presets = {
+        '1': 50, '2': 100, '3': 250, '4': 500, '5': 1000
+    }
+
+    print("Choose a CPS option:")
+    for key, value in presets.items():
+        print(f"{key}. {value} CPS")
+
     while True:
-        user_cps = input("Enter Desired CPS(Clicks Per Second) Speed (1 - 1000): ") # Get user input for desired CPS speed
-        if 1 <= int(user_cps) <= 1000: # Check if the user input is within the valid range (1 - 1000)
+        choice = input("Enter your choice (1-5) or '6' for Custom: ")
+
+        if choice == '6':
+            # Custom option: Get user input for desired CPS speed
+            while True:
+                user_cps = input("Enter Desired CPS(Clicks Per Second) Speed (1 - 1000): ")
+                if 1 <= int(user_cps) <= 1000:
+                    break
+                else:
+                    print("Value must be between 1 and 1000!")
+            set_speed = 1 / int(user_cps)  # Calculate the time interval between clicks based on desired CPS
+            break
+        elif choice in presets:  # Use the selected preset option
+            user_cps = presets[choice]
+            set_speed = 1 / user_cps
             break
         else:
-            print("Value must be between 1 and 1000!")
-    set_speed = 1 / int(user_cps) # Calculate the time interval between clicks based on desired CPS
-    print(f'Speed set to {user_cps} CPS. Time between clicks: {set_speed:.4f}') # Display the set CPS speed and time interval
+            print("Invalid choice. Please choose a valid option (1-6).")
+
+    print(f'Speed set to {user_cps} CPS. Time between clicks: {set_speed:.4f}')  # Display the set CPS speed and time interval
     return set_speed
+
 
 # Function to handle the 'c' key press event
 def on_press(event):
@@ -52,7 +74,7 @@ def custom_event_loop(cps):
         if keyboard.is_pressed('q'): # Check for stop signal (for example, pressing the "q" key)
             keyboard.unhook_all() if listener else keyboard.on_press(on_press) # Toggle the listener state and print status
             listener = not listener
-            print('Listener:', 'Off' if listener else 'On')
+            print('Listener:', 'On' if listener else 'Off')
             time.sleep(0.5)
 
 if __name__ == "__main__":
